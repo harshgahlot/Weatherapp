053128692eaa91a105b3d1c549c32c72f24867a9
harshgahlot <86676479+harshgahlot@users.noreply.github.com>
2022-10-02 19:32:01 +0530

    Add files via upload

---
+++ weather-forecast-master/src/components/CityForm.js
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+import styles from "./CityForm.module.css";
+
+
+const CityForm = (props) => {
+  const cityInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredCity = cityInputRef.current.value;
+    props.onSubmitHandler(enteredCity);
+  };
+
+  return (
+    <>
+      <form className={styles["form-control"]} onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="city">Enter your city</label>
+          <input
+            id="city"
+            type="text"
+            ref={cityInputRef}
+            placeholder="Delhi"
+          />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default CityForm;
